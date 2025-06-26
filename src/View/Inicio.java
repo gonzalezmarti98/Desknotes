3a5c25f684ef51dce272104ecc6d1d005b551113
Marti Gonzalez <martimartigonzalez@gmail.com>
2025-06-26 16:57:54 +0200

    login con lista lateral correcta

--- src/View/Inicio.java
+++ src/View/Inicio.java
@@ -19,6 +19,9 @@ public class Inicio extends javax.swing.JFrame {
         
         // añadimos notas a la lista en forma de texto
         modeloNotas.addElement("· "+"Título de nota 1");
+        modeloNotas.addElement("· "+"Título de nota 2");
+        modeloNotas.addElement("· "+"Título de nota 3");
+        modeloNotas.addElement("· "+"Título de nota 4");
         
         // Evento al seleccionar un título de nota
         listaNotas.addListSelectionListener(e -> {
@@ -41,71 +44,59 @@ public class Inicio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        listaNotas = new javax.swing.JList<>();
+        PanelLista = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        listaNotas = new javax.swing.JList<>();
+        background = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
-
-        jLabel2.setText("jLabel2");
-
-        listaNotas.setBackground(new java.awt.Color(153, 153, 153));
-        listaNotas.setBorder(null);
-        listaNotas.setForeground(new java.awt.Color(0, 0, 0));
-        listaNotas.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "lista de notas . . ." };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane1.setViewportView(listaNotas);
+        PanelLista.setBackground(new java.awt.Color(204, 204, 204));
+        PanelLista.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 0, 0));
         jLabel1.setText("Mis Notas");
+        PanelLista.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(49, 43, -1, -1));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(113, 113, 113)
-                        .addComponent(jLabel2)))
-                .addContainerGap(537, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(52, 52, 52)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
-                .addContainerGap(107, Short.MAX_VALUE))
-        );
+        jScrollPane2.setBackground(new java.awt.Color(213, 195, 156));
+
+        listaNotas.setBackground(new java.awt.Color(178, 165, 146));
+        listaNotas.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane2.setViewportView(listaNotas);
+
+        PanelLista.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(49, 74, 110, 300));
+
+        background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/fondo mosaico v2.jpg"))); // NOI18N
+        background.addContainerListener(new java.awt.event.ContainerAdapter() {
+            public void componentRemoved(java.awt.event.ContainerEvent evt) {
+                backgroundComponentRemoved(evt);
+            }
+        });
+        PanelLista.add(background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 630, 420));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(PanelLista, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(PanelLista, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void backgroundComponentRemoved(java.awt.event.ContainerEvent evt) {//GEN-FIRST:event_backgroundComponentRemoved
+        // TODO add your handling code here:
+    }//GEN-LAST:event_backgroundComponentRemoved
 
     /**
      * @param args the command line arguments
@@ -143,10 +134,10 @@ public class Inicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel PanelLista;
+    private javax.swing.JLabel background;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JList<String> listaNotas;
     // End of variables declaration//GEN-END:variables
 }
