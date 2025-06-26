b8f6117599d09c3f03bad4fa65e4b2dc5d010866
Marti Gonzalez <martimartigonzalez@gmail.com>
2025-06-26 15:57:36 +0200

    View de login y inicio

---
+++ src/Main/MainClass.java
@@ -0,0 +1,17 @@
+
+package Main;
+
+/**
+ *
+ * @author Martí González
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
